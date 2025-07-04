189334bfd78891e07ab6ac1c9463ccd2572ad396
samedit66 <samedit66@yandex.ru>
2025-07-04 14:14:57 +0300

    fix(create_admin): skip creation when admin already exists

--- colossus/apps/accounts/management/commands/create_admin.py
+++ colossus/apps/accounts/management/commands/create_admin.py
@@ -12,12 +12,12 @@ class Command(BaseCommand):
         password = settings.ADMIN_PASS
         email = settings.ADMIN_EMAIL
 
-        if not username or not password:
-            self.stdout.write(self.style.WARNING('ADMIN_LOGIN or ADMIN_PASS not set, skipping admin creation.'))
+        if User.objects.exists():
+            self.stdout.write(self.style.WARNING(f'Admin user already exists, skipping creation.'))
             return
 
-        if User.objects.filter(username=username).exists():
-            self.stdout.write(self.style.WARNING(f'Admin user "{username}" already exists, skipping creation.'))
+        if not username or not password:
+            self.stdout.write(self.style.WARNING('ADMIN_LOGIN or ADMIN_PASS not set, skipping admin creation.'))
             return
 
         User.objects.create_superuser(username=username, password=password, email=email)
